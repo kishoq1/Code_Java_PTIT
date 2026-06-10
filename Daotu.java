aee7a23e11cb9994c608460068492719dd77d3d1
QuanPVT <anquan0298@gmail.com>
2026-06-10 08:54:44 +0700

    Create Daotu.java

---
+++ Daotu.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CodePTIT;
+import java.util.Scanner;
+/**
+ *
+ * @author Kisho
+ */
+public class Daotu {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        input.nextLine();
+        while (t-- > 0) {
+            String[] s = input.nextLine().split("\\s+");
+            for (int i = 0; i < s.length; i++) {
+                s[i] = new StringBuilder(s[i]).reverse().toString();
+            }
+            for (String i : s)
+                System.out.print(i + " ");
+            System.out.println();
+        }
+    }
+}
