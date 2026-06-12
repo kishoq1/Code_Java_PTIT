3062943c860a563dda4672362f1fba70525a2f6e
QuanPVT <anquan0298@gmail.com>
2026-06-12 09:02:18 +0700

    Create Hieusonguyenlon2.java

---
+++ Hieusonguyenlon2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CodePTIT;
+import java.util.Scanner;
+/**
+ *
+ * @author Kisho
+ */
+public class Hieusonguyenlon2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextBigInteger().subtract(input.nextBigInteger()));
+    }
+}
