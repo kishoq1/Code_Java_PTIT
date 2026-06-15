ac5155a9dafe3712941896204da4d8c721bce2ff
QuanPVT <anquan0298@gmail.com>
2026-06-15 09:57:34 +0700

    Create Sodep1.java

---
+++ Sodep1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CodePTIT;
+import java.util.Scanner;
+/**
+ *
+ * @author Kisho
+ */
+public class Sodep1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        while (t-- > 0) {
+            StringBuffer s = new StringBuffer(sc.nextLine());
+            System.out.println(s.toString().matches("[^13579]+") && s == s.reverse() ? "YES" : "NO");
+        }
+    }
+}
