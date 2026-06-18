4c36a39e9e1b5a5296fcb037380ef01b9df9e7ec
QuanPVT <anquan0298@gmail.com>
2026-06-18 16:20:11 +0700

    Create Tinhtong.java

---
+++ Tinhtong.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CodePTIT;
+
+/**
+ *
+ * @author Kisho
+ */
+import java.util.Scanner;
+
+public class Tinhtong {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        while (t-- > 0) {
+            long n = input.nextLong();
+            System.out.println(n * (n + 1) / 2);
+        }
+        input.close();
+    }
+}
