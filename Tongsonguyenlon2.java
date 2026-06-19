3143b6884d81171e745e1443a8a20dbb917d5c0e
QuanPVT <anquan0298@gmail.com>
2026-06-19 17:13:44 +0700

    Create Tongsonguyenlon2.java

---
+++ Tongsonguyenlon2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CodePTIT;
+import java.util.Scanner;
+/**
+ *
+ * @author Kisho
+ */
+public class Tongsonguyenlon2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextBigInteger().add(sc.nextBigInteger()));
+        sc.close();
+    }
+}
